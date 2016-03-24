c653dd8e6721f477621168ed0e6010be07c07aa7
Warren S <mr.dude1848@gmail.com>
2016-03-23 23:20:45 -0400

    Moved Mac-specific initializations to another class.

---
+++ app/src/scribbles/MacInit.java
@@ -0,0 +1,25 @@
+package scribbles;
+
+import com.apple.eawt.AppEvent;
+import com.apple.eawt.Application;
+import com.apple.eawt.QuitResponse;
+import scribbles.gui.AboutDialog;
+import scribbles.gui.PreferencesDialog;
+import scribbles.gui.ScribbleApplication;
+
+class MacInit
+{
+	static void initMacStuff()
+	{
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Scribbles"); //set the application title on Mac OSX
+		System.setProperty("apple.laf.useScreenMenuBar", "true"); //use the menu on Mac OSX
+
+		Application.getApplication().setAboutHandler( (aboutEvent) -> AboutDialog.show(null) );
+		Application.getApplication().setPreferencesHandler( (preferencesEvent) -> PreferencesDialog.show() );
+		Application.getApplication().setQuitHandler( (AppEvent.QuitEvent quitEvent, QuitResponse quitResponse) -> {
+			//don't quit the application, but send a quit message to all open windows
+			quitResponse.cancelQuit();
+			ScribbleApplication.requestCloseOnAllWindows();
+		});
+	}
+}
